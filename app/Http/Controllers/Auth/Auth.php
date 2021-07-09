1b972df31fd2a5ac767189189f28c0001d1c8649
Halil Hakan Karabay <hakankarabay@icloud.com>
2021-07-09 16:28:37 +0300

    Auth completed

--- app/Http/Controllers/Auth/Auth.php
+++ app/Http/Controllers/Auth/Auth.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\AuthRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Redirector;
 
 class Auth extends Controller
@@ -37,8 +39,7 @@ class Auth extends Controller
         if ($user) {
             $user = User::all()->last();
             \Auth::login($user, true);
-            $this->setFlash('Başarıyla kayıt olundu!');
-            return redirect(route('admin.home'));
+            return $this->successLogin();
         }
 
         $this->setFlash('Veritabanı bağlantı hatası!');
@@ -46,10 +47,12 @@ class Auth extends Controller
 
     }
 
+
     /**
      * @param AuthRequest $request
+     * @return \Illuminate\Http\RedirectResponse|Redirector
      */
-    public function login(AuthRequest $request)
+    public function login(AuthRequest $request) : Redirector | RedirectResponse
     {
 
         $credentials = [
@@ -59,9 +62,43 @@ class Auth extends Controller
         ];
 
         if (\Auth::attempt($credentials, true)) {
-            return;
+
+            return $this->successLogin();
+
         }
 
+        return redirect()->back()
+            ->withInput($request->only('email'))
+            ->withErrors([
+                'password' => 'E-posta ya da parola yanlış!'
+            ]);
+
     }
+
+    /**
+     * @return Redirector
+     */
+    public function successLogin() : Redirector
+    {
+
+        $this->setFlash('Başarıyla giriş yapıldı!');
+        return redirect(route('admin.home'));
+
+    }
+
+    public function logout(Request $request)
+    {
+
+        \Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect(route('admin.login'));
+
+
+    }
+
 
 }
